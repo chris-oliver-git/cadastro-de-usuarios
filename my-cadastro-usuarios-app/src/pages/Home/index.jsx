5907372120b7cf28ff159fa4cf5f5b1383ab514a
Christopher Silva <109239723+chris-oliver-git@users.noreply.github.com>
2025-07-04 20:47:52 -0300

    estilizando o CSS

--- my-cadastro-usuarios-app/src/pages/Home/index.jsx
+++ my-cadastro-usuarios-app/src/pages/Home/index.jsx
@@ -1,14 +1,52 @@
 import './style.css'
+import Excluir from '../../assets/excluir.png'
+
 function Home() {
+  const users = [
+    {
+      id:'52453453453',
+      name: 'Christopher',
+      age: 33,
+      email: 'christopher@email.com'
+    },
+    {
+      id:'52453465623',
+      name: 'Julliana',
+      age: 33,
+      email: 'julliana@email.com'
+    },
+    {
+      id:'7785465623',
+      name: 'Daniel',
+      age: 25,
+      email: 'daniel@email.com'
+    }
+  ]
   return (
       <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text'/>
-        <input name='idade' type='number'/>
-        <input name='email' type='email'/>
+        <input placeholder= "Nome" name='nome' type='text'/>
+        <input placeholder= "Idade" name='idade' type='number'/>
+        <input placeholder= "E-mail" name='email' type='email'/>
         <button type='button'>Cadastrar</button>
-      </form> 
+      </form>
+      
+      {users.map((user) => (
+      <div key={user.id} className='card'>
+        <div>
+        <p>Nome: {user.name} </p>
+        <p>Idade: {user.age} </p>
+        <p>E-mail: {user.email} </p>
+        </div>
+        <button>
+          <img src={Excluir}/>
+        </button>
+      </div>
+      
+      ))}
+      
+
       </div>
   )  
 }
